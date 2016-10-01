2fc92ee80ecba9b6bf877de2721cfca97e676555
Your Name <you@example.com>
2016-10-01 23:42:38 +0300

    3. Commit Patronlar eklendi

---
+++ TelefonDefteri/Patronlar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonDefteri
+{
+    class Patronlar
+    {
+        //3. Class eklendi
+    }
+}
