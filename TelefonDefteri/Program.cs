b5cb148ec38a4e04381c1d0e6fb1e620a45847c1
Your Name <you@example.com>
2016-10-02 03:05:09 +0300

    Maini değişti sıyırcam

--- TelefonDefteri/Program.cs
+++ TelefonDefteri/Program.cs
@@ -16,7 +16,7 @@ namespace TelefonDefteri
             //Navigasyoon geliştirmesi yaptık. 2. branchde
 
 
-            //masterdaqweqqqq
+            //mqqqqqqqqqqqqqqqqqeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
         }
     }
 }
